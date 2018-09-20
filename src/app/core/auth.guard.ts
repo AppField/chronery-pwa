8eaa750226aa661e4cad2145142a3576843ca7e8
Roman Mühlfeldner <roman@muehlfeldner.net>
2018-09-20 14:33:11 +0200

    Added toast message when user is not logged in.

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -3,13 +3,15 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { map, take, tap } from 'rxjs/operators';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private auth: AuthService, private router: Router) {
+  constructor(private auth: AuthService, private router: Router,
+              private toastCtrl: ToastController) {
   }
 
   canActivate(
@@ -23,8 +25,20 @@ export class AuthGuard implements CanActivate {
         if (!loggedIn) {
           console.log('not logged in');
           this.router.navigate(['/authenticate']);
+          this.showSignInToast();
         }
       })
     );
+  }
+
+  private async showSignInToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'Bitte melden Sie sich an, um Chronery nutzen zu können.',
+      duration: 5000,
+      showCloseButton: true,
+      position: 'bottom',
+      closeButtonText: 'OK'
+    });
+    toast.present();
   }
 }
