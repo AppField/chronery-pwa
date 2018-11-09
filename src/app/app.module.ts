100935d853b03d3e2a1a7dd1ab5f0974e60eb1de
Roman Mühlfeldner <roman@muehlfeldner.net>
2018-11-09 17:46:55 +0100

    Registered locale De

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
@@ -11,6 +11,10 @@ import { environment, firebaseConfig } from '../environments/environment';
 import { WindowRefService } from './services/window-ref.service';
 import { AngularFireModule } from '@angular/fire';
 import { CoreModule } from './core/core.module';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe);
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,6 +29,7 @@ import { CoreModule } from './core/core.module';
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
+    { provide: LOCALE_ID, useValue: 'de' },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     WindowRefService
   ],
